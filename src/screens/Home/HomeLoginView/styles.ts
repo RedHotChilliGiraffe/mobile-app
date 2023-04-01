45a961a1b14f2afe4f8264c87b0f258f2e001631
Inevera11 <zuzannabozek2002@gmail.com>
2023-04-01 23:28:01 +0200

    backgroundGiraffe

---
+++ src/screens/Home/HomeLoginView/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const unregisterStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    marginTop: 80,
+    marginBottom: 40,
+    justifyContent: 'center',
+    width: 300,
+    height: 300,
+  },
+  buttons: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+});
